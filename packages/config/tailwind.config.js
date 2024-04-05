bfd8ac3c826e6856eaca623a89becb6ad3403bff
nameRoy <1604829774@qq.com>
2024-04-05 15:10:22 +0800

    feat: 搭建 monorepo 项目结构

---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1677ff",
+        success: "#52c41a",
+        warning: "#faad14",
+        danger: "#ff4d4f",
+        info: "#1677ff",
+      },
+    },
+  },
+};
--- packages/utils/is-module-exist.ts
+++ packages/utils/is-module-exist.ts
@@ -1,7 +1,7 @@
 import fs from "node:fs";
 import path from "node:path";
 
-export function isModuleExist(moduleName) {
+export function isModuleExist(moduleName: string) {
   const root = process.cwd();
   const modulesPath = path.resolve(root, "node_modules", moduleName);
   return fs.existsSync(modulesPath);
